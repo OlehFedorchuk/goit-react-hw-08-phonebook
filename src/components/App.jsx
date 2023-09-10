509f33fbf93d03e855ce1102ba7de1a89668f67c
OLEH FEDORCHUK <fedorchuk2018@gmail.com>
2023-09-10 19:25:45 +0100

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,71 @@
+import NotFoundPage from 'pages/NotFoundPage/NotFoundPage';
+import { lazy } from 'react';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Route, Routes } from 'react-router-dom';
+import { authOperations } from 'redux/auth/auth-operations';
+import authSelectors from 'redux/auth/auth-selectors';
+import { AppBar } from './AppBar/AppBar';
+import Layout from './Layout/Layout';
+import Loader from './Loader/Loader';
+import PrivateRoute from './PrivateRoute';
+import RestrictedRoute from './RestrictedRoute';
+
+const HomePage = lazy(() => import('../pages/HomePage/HomePage'));
+const RegisterPage = lazy(() => import('../pages/RegisterPage/RegisterPage'));
+const LoginPage = lazy(() => import('../pages/LoginPage/LoginPage'));
+const ContactsPage = lazy(() => import('../pages/ContactsPage/ContactsPage'));
+
 export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+  const dispatch = useDispatch();
+
+  const isRefreshingUser = useSelector(authSelectors.getIsRefreshingUser);
+
+  const userRefreshed = useSelector(authSelectors.getUserRefreshed);
+
+  useEffect(() => {
+    // Виконує оновлення користувача лише раз, якщо він ще не був оновлений
+    if (!userRefreshed) {
+      dispatch(authOperations.refreshUser());
+    }
+  }, [dispatch, userRefreshed]);
+
+  return !isRefreshingUser ? (
+    <>
+      <AppBar />
+
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<HomePage />} />
+          <Route
+            path="register"
+            element={
+              <RestrictedRoute redirectTo="/contacts">
+                <RegisterPage />
+              </RestrictedRoute>
+            }
+          />
+          <Route
+            path="login"
+            element={
+              <RestrictedRoute redirectTo="/contacts">
+                <LoginPage />
+              </RestrictedRoute>
+            }
+          />
+          <Route
+            path="/contacts"
+            element={
+              <PrivateRoute redirectTo="/login">
+                <ContactsPage />
+              </PrivateRoute>
+            }
+          />
+          <Route path="*" element={<NotFoundPage />} />
+        </Route>
+      </Routes>
+    </>
+  ) : (
+    <Loader />
   );
 };
