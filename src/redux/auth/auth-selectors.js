971c5769ebbcce7e763ea5e5b452bf4f82558356
OLEH FEDORCHUK <fedorchuk2018@gmail.com>
2023-09-11 23:14:53 +0100

    update

--- src/redux/auth/auth-selectors.js
+++ src/redux/auth/auth-selectors.js
@@ -1,6 +1,6 @@
 const getIsLoggedIn = state => state.auth.isLoggedIn;
 
-const getUserName = state => state.auth.user;
+const getUserName = state => state.auth.user.name;
 
 const getIsRefreshingUser = state => state.auth.isRefreshingUser;
 
